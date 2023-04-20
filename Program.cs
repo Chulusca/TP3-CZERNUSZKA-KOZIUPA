74f3bb107b7bc30c21a817cabe6d05e76e21b43e
LUCAS NICOLAS KOZIUPA <47572827@ort>
2023-04-20 08:35:54 -0300

    terminado

--- Program.cs
+++ Program.cs
@@ -16,21 +16,20 @@ do{
     switch (opcion)
         {
             case 1:
-            Console.Clear();
                 id = Ticketera.AgregarCliente(IngresarCliente());
                 Console.WriteLine($"Su id de entrada es {id}");
+                Console.ReadLine();
                 break;
             case 2:
-                Console.Clear();
                 ImprimirEstadisticas();
                 break;
             case 3:
-                Console.Clear();
                 MostrarCliente(Funciones.IngresarEntero("Ingrese el id de entrada a buscar"));
+                Console.ReadLine();
                 break;
             case 4:
-                Console.Clear();
                 PudoCambiarEntrada();
+                Console.ReadLine();
                 break;
             case 5:
                 break;
@@ -40,17 +39,21 @@ do{
 }while(opcion !=5); 
 
 Cliente IngresarCliente(){
-    int dni = Funciones.IngresarDni("Ingrese el dni del cliente");
-    string apellido = Funciones.IngresarTexto("Ingrese el apellido del cliente");
-    string nom = Funciones.IngresarTexto("Ingrese el nombre del cliente");
+    int dni = Funciones.IngresarDni("Ingrese el dni del cliente ");
+    string apellido = Funciones.IngresarTexto("Ingrese el apellido del cliente ");
+    string nom = Funciones.IngresarTexto("Ingrese el nombre del cliente ");
     int TipoDeEntrada = Funciones.IngresarEnteroEnRango("Ingrese el tipo de entrada",1 , 4);
     
     return new Cliente(dni, apellido, nom, DateTime.Today, TipoDeEntrada, dia[TipoDeEntrada]);
 }
 void ImprimirEstadisticas(){
-    foreach(string t in estadisticas){
-        Console.WriteLine(t);
-    }
+    estadisticas = Ticketera.EstadisticasTicketera();
+    if(estadisticas[0] != null){
+        foreach(string t in estadisticas){
+            Console.WriteLine(t);
+        }
+    }else Console.WriteLine("Todavia no se ingresaron clientes");
+    Console.ReadLine();
 }
 void MostrarCliente(int id){
     Cliente cliente = Ticketera.BuscarCliente(id);
